052c6f4a29d54a7879be3bd727607a86cedf9f1b
m9china <nqobilencaza2@gmail.com>
2025-06-14 00:25:26 +0200

    fix: update metadata in layout and enhance AboutUs component structure

--- src/components/About/AboutUs.tsx
+++ src/components/About/AboutUs.tsx
@@ -1,7 +1,8 @@
 export const AboutUs = () => {
   return (
     <main className="pt-[5rem] px-4 bg-white min-h-screen" aria-labelledby="about-us page">
-      <section>
+      <h1 className="text-4xl font-bold text-[#009ec5]">Who We Are</h1>
+      <section className="mt-4 space-y-2">
         <p className="text-black">
           NineZero Holdings (PTY) LTD is not a manufacture but a distributor. It
           has strategically formed alliences with some recognised SA Fuel oil
