f7561b68d42ff8be03399bb7931ffd931950fab8
m9china <nqobilencaza2@gmail.com>
2025-06-14 01:26:27 +0200

    fix: adjust layout overflow and image width in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="pt-[4rem] pb-[2.8rem] min-h-screen bg-white" aria-labelledby="home-page">
+    <main className="pt-[4rem] pb-[2.8rem] overflow-hidden min-h-screen bg-white" aria-labelledby="home-page">
       <div className="relative">
         <Image
           src="/sploosh.gif"
           alt="Hero Image"
           width={1920}
           height={1080}
-          className="w-screen h-auto object-cover scale-150"
+          className="w-full h-auto object-cover scale-150"
         />
         <div className="absolute inset-0 px-2 flex items-center justify-center">
           <h1 className="text-white text-4xl font-bold text-center">
