05b96b2fb56476b3b29b6aff7f194f6e75e5fdca
m9china <nqobilencaza2@gmail.com>
2025-06-14 00:49:14 +0200

    fix: update Home component for correct company name and ensure full height layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="pt-[4rem] pb-[2.8rem] bg-white" aria-labelledby="home-page">
+    <main className="pt-[4rem] pb-[2.8rem] min-h-screen bg-white" aria-labelledby="home-page">
       <div className="relative">
         <Image
           src="/sploosh.gif"
@@ -12,8 +12,8 @@ export default function Home() {
           className="w-full h-auto object-cover scale-150"
         />
         <div className="absolute inset-0 flex items-center justify-center">
-          <h1 className="text-white text-3xl font-bold text-center">
-            NineZero Holdings Pty Ldt
+          <h1 className="text-white text-4xl font-bold text-center">
+            NineZero Holdings Pty Ltd
           </h1>
           <h1>
 
