93dfe59b595aaefd0df17d17ea7377bbc54d6d42
m9china <nqobilencaza2@gmail.com>
2025-06-18 19:49:25 +0200

    feat: add Mission component to showcase company mission and vision

---
+++ src/components/Home/Mission.tsx
@@ -0,0 +1,23 @@
+export const Mission = () => {
+  return (
+    <section className="bg-white py-16">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <h2 className="text-3xl font-bold text-[#009ec5] mb-6">Our Mission</h2>
+        <div className="text-gray-700 text-md flex items-center">
+          <p>To be the first choice level BBBEE distributor 
+            of petroleum and gas products in Southern Africa, providing exceptional 
+            service and quality products that empower our customers and communities.</p>
+          
+        </div>
+        <h2 className="text-3xl font-bold text-[#009ec5] mt-6">Our Vision</h2>
+        <div className="text-gray-700 text-md flex items-center mt-5">
+          <p>To provide quality products and competitive prices in a profitable manner
+            including flexibility, reliability of supply, accessibilty, consostency, simplicity
+            and personal service to all our clients
+          </p>
+          
+        </div>
+      </div>
+    </section>
+  );
+}
