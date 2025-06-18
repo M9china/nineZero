659b7789a8dad9dc6fc8b3d9f8047bb06d849b8e
m9china <nqobilencaza2@gmail.com>
2025-06-18 19:49:49 +0200

    feat: add slides data for product images in ProductsStatic component

---
+++ src/components/Products/ProductsStatic.ts
@@ -0,0 +1,18 @@
+export const slides = [
+  {
+    src: "/product1.jpeg",
+    alt: "Product 1",
+  },
+  {
+    src: "/product2.jpeg",
+    alt: "Product 2",
+  },
+  {
+    src: "/product3.jpeg",
+    alt: "Product 3",
+  },
+  {
+    src: "/product4.jpeg",
+    alt: "Product 4",
+  },
+];
