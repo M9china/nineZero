1d9acf5b16322f87cbb5a9d392b1ce221e923381
m9china <nqobilencaza2@gmail.com>
2025-06-24 16:35:25 +0200

    fix: enhance Mission component with structured values section and improve text formatting

--- src/components/Home/Mission.tsx
+++ src/components/Home/Mission.tsx
@@ -4,20 +4,139 @@ export const Mission = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <h2 className="text-3xl font-bold text-[#009ec5] mb-6">Mission</h2>
         <div className="text-white text-md flex items-center">
-          <p>To be the first choice level BBBEE distributor, to
-            supply Municipalities, Mining, Manufacturing, Tyre industry, Sugar and 
-            Power generations industries with iols and all petroleum related products.</p>
-          
+          <p>
+            To be the first choice level BBBEE distributor, to supply
+            Municipalities, Mining, Manufacturing, Tyre industry, Sugar and
+            Power generations industries with iols and all petroleum related
+            products.
+          </p>
         </div>
         <h2 className="text-3xl font-bold text-[#009ec5] mt-6">Vision</h2>
         <div className="text-white text-md flex items-center mt-5">
-          <p>To provide quality products and competitive prices in a profitable manner
-            including flexibility, reliability of supply, accessibilty, consostency, simplicity
-            and personal service to all our clients
+          <p>
+            To provide quality products and competitive prices in a profitable
+            manner including flexibility, reliability of supply, accessibilty,
+            consostency, simplicity and personal service to all our clients
           </p>
-          
+        </div>
+        <div>
+          <h2 className="text-3xl font-bold text-[#009ec5] mt-6">Values</h2>
+          <div className="text-white text-md flex items-center mt-5">
+            <span className="flex items-center border border-white rounded-full px-1 py-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="size-2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                />
+              </svg>
+            </span>
+            <p className="ml-2">Partnership</p>
+          </div>
+          <div className="text-white text-md flex items-center mt-5">
+            <span className="flex items-center border border-white rounded-full px-1 py-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="size-2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                />
+              </svg>
+            </span>
+            <p className="ml-2">Respect</p>
+          </div>
+          <div className="text-white text-md flex items-center mt-5">
+            <span className="flex items-center border border-white rounded-full px-1 py-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="size-2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                />
+              </svg>
+            </span>
+            <p className="ml-2">Costomer Service Response</p>
+          </div>
+          <div className="text-white text-md flex items-center mt-5">
+            <span className="flex items-center border border-white rounded-full px-1 py-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="size-2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                />
+              </svg>
+            </span>
+            <p className="ml-2">Partnership</p>
+          </div>
+          <div className="text-white text-md flex items-center mt-5">
+            <span className="flex items-center border border-white rounded-full px-1 py-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="size-2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                />
+              </svg>
+            </span>
+            <p className="ml-2">Ethical Practices</p>
+          </div>
+          <div className="text-white text-md flex items-center mt-5">
+            <span className="flex items-center border border-white rounded-full px-1 py-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="size-2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                />
+              </svg>
+            </span>
+            <p className="ml-2">Acountability</p>
+          </div>
         </div>
       </div>
     </section>
   );
-}
+};
